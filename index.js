6b1819db3ba602315f6bd37a8f4ee67ad757dc1b
Teresia <teresiamwangi26@gmail.com>
2023-03-14 12:21:05 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function scuberGreetingForFeet(){
+/*function scuberGreetingForFeet(){
   // Write your code here!
 }
 
@@ -8,4 +8,46 @@ function ternaryCheckCity(){
 
 function switchOnCharmFromTip(){
   // Write your code here!
+} */
+
+
+
+function scuberGreetingForFeet(value){
+  if( value<= 400) {
+    return 'This one is on me!'
+  } else if(value <= 2500) {
+    return 'I will gladly take your thirty bucks.'
+  }
+  else {
+      return 'No can do.'
+  }
+  }
+
+const city = 'NYC'
+
+function ternaryCheckCity(destination){
+ return (destination === city? "Ok, sounds good.":"No go.")
+}
+
+/*let tip = generous;
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return "Thank you so much.";
+    case  'generous':
+      return "Thank you.";
+    default:
+      return 'Bye.';
+
+  }
+}*/
+function switchOnCharmFromTip(tipAmount){
+  if(tipAmount==='generous'){
+    return'Thank you so much.';
+  }else if (tipAmount==='not as generous'){
+    return'Thank you.';
+  } else{
+    return 'Bye.';
+  }
 }
